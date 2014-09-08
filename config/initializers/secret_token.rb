967292851edf7569d047807dff6eb289204ba9ff
richie serna <richie@balancedpayments.com>
2014-09-07 17:02:39 -0700

    Day 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheedle::Application.config.secret_token = '609ef71cf90a60ff44868aa40726283712192a0127f5137b5aca7206400065e7fb7fc6842785b76900f2250525803106e13dff7ddaf5a9a23577398725992c98'
