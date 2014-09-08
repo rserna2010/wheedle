7cbba5321a298fe4b26a6f8783bed0a049fbf5b7
richie serna <richie@balancedpayments.com>
2014-09-07 17:21:33 -0700

    Add precompiled assets for Heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,28 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Wheedle::Application.config.secret_token = '609ef71cf90a60ff44868aa40726283712192a0127f5137b5aca7206400065e7fb7fc6842785b76900f2250525803106e13dff7ddaf5a9a23577398725992c98'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Wheedle::Application.config.secret_token = secure_token
